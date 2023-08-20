463d55b7c5d514d33dee238237a82e475d4a7cc0
hyeg0121 <hyeg0121@naver.com>
2023-08-20 21:30:53 +0900

    feat: 플레이어와 적 그리기

---
+++ js/main.js
@@ -0,0 +1,34 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+canvas.setAttribute('width', window.innerWidth);
+canvas.setAttribute('height', window.innerHeight);
+
+var player = {
+    x: 100,
+    y: 800,
+    width: 50, 
+    height: 50,
+    draw(){
+        ctx.fillStyle = 'green';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+class Obstacle {
+    constructor() {
+        this.x = 500;
+        this.y = 800;
+        this.width = 50;
+        this.height = 50;
+    }
+
+    draw() {
+        ctx.fillStyle = 'red';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+var obstacle = new Obstacle;
+obstacle.draw();
+
