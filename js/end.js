1062f41220ee1c0be4eb36b1ea6ba440f991a0ae
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-29 18:34:16 +0900

    refactor: 리소스 변경 및 밸런스 조정

--- js/end.js
+++ js/end.js
@@ -4,3 +4,10 @@ let score = new URL(location.href).searchParams.get('score');
 
 scoreH2.innerHTML = `SCORE : ${score}`;
 
+
+const bgm = new Audio('./resource/music/fail.ogg');
+window.onload = () => {
+    bgm.muted = true;
+    bgm.play();
+    bgm.muted = false;
+}
