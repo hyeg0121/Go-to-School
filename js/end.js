30351e4dbeeaf5c6fc5d159516becc84cfca6645
hyeg0121 <w2218@e-mirim.hs.kr>
2024-04-05 22:34:08 +0900

    게임오버되면 결과와 이름 저장

--- js/end.js
+++ js/end.js
@@ -1,13 +1,46 @@
 const scoreH2 = document.getElementsByClassName('score')[0];
+let score = Number(new URL(location.href).searchParams.get('score'));
+let name = new URL(location.href).searchParams.get('name');
 
-let score = new URL(location.href).searchParams.get('score');
+const saveData = async () => {
+    try {
+        const request = {
+            name: name,
+            score: Number(score)
+        };
+
+        const response = await fetch('http://localhost:8080/ranking', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(request)
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to save data');
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error('Error while saving data:', error);
+    }
+};
+
+saveData().then(() => {
+    scoreH2.innerHTML = `SCORE : ${score}`;
+}).catch((error) => {
+    console.error('Error:', error);
+});
 
 scoreH2.innerHTML = `SCORE : ${score}`;
 
-
-const bgm = new Audio('./resource/music/fail.ogg');
+// bgm 재생
 window.onload = () => {
-    bgm.muted = true;
-    bgm.play();
-    bgm.muted = false;
+    const bgm = new Audio('../resources/music/fail.ogg');
+    window.onload = () => {
+        bgm.play();
+    };
+
 }
